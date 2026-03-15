877d9af0a07773b798cef2a0f1a152f8bc50cfcd
Sean P <40479293+spacker15@users.noreply.github.com>
2026-03-15 16:50:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default function WeatherPanel({weather}){
-
-return(
-<div className="panel">
-
-<h3>Weather Command</h3>
-
-<div>Temp: {weather.temp}°F</div>
-<div>Conditions: {weather.conditions}</div>
-<div>Wind: {weather.wind}</div>
-
-</div>
-)
-}
+module.exports = nextConfig;
